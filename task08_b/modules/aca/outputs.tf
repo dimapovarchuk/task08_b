bf376ea1b1ee582d204a8d9a2e8d334d4412893e
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2025-03-26 11:31:55 +0000

    Added changes

--- task08_b/modules/aca/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "aca_fqdn" {
-  description = "The FQDN for the Azure Container App"
-  value       = azurerm_container_app.container_app.latest_revision_fqdn
-}
-
--- task08_b/modules/k8s/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aks_lb_ip" {
-  description = "IP address of the App Load Balancer"
-  value       = data.kubernetes_service.service.status.0.load_balancer.0.ingress.0.ip
-}
