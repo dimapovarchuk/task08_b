1b441d01a6a68e044d7f960d65515ce4c648c89d
dmytro_povarchuk <Dmytro_Povarchuk@epam.com>
2025-03-26 11:55:03 +0000

    Added changes

--- task08_b/modules/k8s/main.tf
+++ task08_b/modules/k8s/main.tf
@@ -1,11 +1,3 @@
-# Створюємо namespace
-resource "kubernetes_namespace" "default" {
-  metadata {
-    name = "default"
-  }
-}
-
-# Створюємо SecretProviderClass
 resource "kubectl_manifest" "secret-provider" {
   yaml_body = templatefile("${path.root}/k8s-manifests/secret-provider.yaml.tftpl", {
     aks_kv_access_identity_id  = var.aks_secret_provider_identity_id
@@ -15,16 +7,17 @@ resource "kubectl_manifest" "secret-provider" {
     redis_password_secret_name = var.redis_primary_key_secret
   })
 
-  depends_on = [kubernetes_namespace.default]
+  wait = true
+  timeouts {
+    create = "2m"
+  }
 }
 
-# Чекаємо після створення SecretProviderClass
 resource "time_sleep" "wait_after_secret_provider" {
   depends_on      = [kubectl_manifest.secret-provider]
   create_duration = "30s"
 }
 
-# Створюємо Deployment
 resource "kubectl_manifest" "deployment" {
   yaml_body = templatefile("${path.root}/k8s-manifests/deployment.yaml.tftpl", {
     acr_login_server = var.acr_login_server
@@ -32,57 +25,46 @@ resource "kubectl_manifest" "deployment" {
     image_tag        = var.image_tag
   })
 
+  wait_for {
+    field {
+      key   = "status.availableReplicas"
+      value = "1"
+    }
+  }
+
   depends_on = [
     kubectl_manifest.secret-provider,
     time_sleep.wait_after_secret_provider
   ]
 }
 
-# Чекаємо поки поди запустяться
-resource "null_resource" "wait_for_pods" {
-  provisioner "local-exec" {
-    command = "kubectl wait --for=condition=ready pod -l app=redis-flask-app -n default --timeout=300s"
-  }
-
-  depends_on = [kubectl_manifest.deployment]
+resource "time_sleep" "wait_after_deployment" {
+  depends_on      = [kubectl_manifest.deployment]
+  create_duration = "30s"
 }
 
-# Створюємо Service
 resource "kubectl_manifest" "service" {
   yaml_body = file("${path.root}/k8s-manifests/service.yaml")
 
+  wait_for {
+    field {
+      key        = "status.loadBalancer.ingress.[0].ip"
+      value      = "^(\\d+(\\.|$)){4}"
+      value_type = "regex"
+    }
+  }
+
   depends_on = [
     kubectl_manifest.deployment,
-    null_resource.wait_for_pods
+    time_sleep.wait_after_deployment
   ]
 }
 
-# Чекаємо поки сервіс отримає IP
 resource "time_sleep" "wait_for_service" {
   depends_on      = [kubectl_manifest.service]
   create_duration = "60s"
 }
 
-# Перевіряємо наявність всіх компонентів
-resource "null_resource" "verify_deployment" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      kubectl get secretproviderclass redis-flask-app-kv-integration -n default
-      kubectl get deployment redis-flask-app -n default
-      kubectl get service redis-flask-app-service -n default
-      kubectl get pods -l app=redis-flask-app -n default
-    EOT
-  }
-
-  depends_on = [
-    kubectl_manifest.secret-provider,
-    kubectl_manifest.deployment,
-    kubectl_manifest.service,
-    time_sleep.wait_for_service
-  ]
-}
-
-# Отримуємо IP сервісу
 data "kubernetes_service" "service" {
   metadata {
     name      = "redis-flask-app-service"
@@ -91,7 +73,6 @@ data "kubernetes_service" "service" {
 
   depends_on = [
     kubectl_manifest.service,
-    time_sleep.wait_for_service,
-    null_resource.verify_deployment
+    time_sleep.wait_for_service
   ]
 }
